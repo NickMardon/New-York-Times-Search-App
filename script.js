34370968c21eb3dc91271f653e85f370f602ef3c
Rees Dart <thedominicparker@gmail.com>
2020-06-22 11:54:36 -0700

    script started

---
+++ script.js
@@ -0,0 +1,40 @@
+// grab IDs from html
+var searchTerm = $("searchTerm");
+var numOfRecs = $("numOfRecs");
+var startYear = $("startYear");
+var endYear = $("endYear");
+var searchBtn = $("searchBtn");
+var topArticles = $("topArticles");
+
+// array to push found articles into
+var articlesArray = []
+
+// when search button clicked, input values taken and used to search api
+searchBtn.on("click", function(event) {
+    event.preventDefault();
+    // take value from searchInput
+    searchTerm = searchTerm.trim()
+    
+})
+
+
+
+
+
+
+
+
+
+
+// When 
+function displayArticles() {
+    // clear articles prior to adding new articles
+    topArticles.empty();
+    // loop through array of found articles
+    for (let i = 0; i < articlesArray.length; i++) {
+        var article = array[i];
+        
+    }
+
+}
+
